2a82f21022b7c27f7cd0fda893dd3e856ba6aa3d
Jonathan Paiva <jonathanapaiva@gmail.com>
2025-02-22 06:12:48 -0300

    feat: add bootstrap for all forms

--- resources/views/attendance.blade.php
+++ resources/views/attendance.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Attendance</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
 <body>
@@ -17,14 +18,20 @@
             @endforeach
         @endif
     </div>
-    <h1>{{ $patient['full_name'] }}</h1>
-    <form action="{{ route("attendance.store") }}" method="post">
-        @csrf
-        @method("post")
-        <textarea name="observations" required></textarea>
-        <input type="hidden" name="consultationId" value="{{ $consultation['id'] }}" required>
-        <input type="submit" value="Enviar">
-    </form>
+    <div class="container py-3">
+        <h1>{{ $patient['full_name'] }}</h1>
+        <form action="{{ route("attendance.store") }}" method="post">
+            @csrf
+            @method("post")
+            <div class="form-group">
+                <label for="observations">Observações</label>
+                <textarea class="form-control" name="observations" placeholder="Digite o que foi definido no atendimento da consulta, como remédios preescritos ou exames..." required></textarea>
+            </div>
+            <input type="hidden" name="consultationId" value="{{ $consultation['id'] }}" required>
+            <input class="my-3 btn btn-primary" type="submit" value="Enviar">
+        </form>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
--- resources/views/consultation.blade.php
+++ resources/views/consultation.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Consultation</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
 <body>
@@ -18,16 +19,30 @@
 
         @endif
     </div>
-    <h1>{{ $doctor['full_name'] }}</h1>
-    <form action="{{ route("consultation.store") }}" method="post">
-        @csrf
-        @method("post")
-        <input type="date" name="date" required> <br>
-        <input type="time" name="time" required> <br>
-        <textarea name="observations" required></textarea>
-        <input type="hidden" name="doctorId" value="{{ $doctor['id'] }}">
-        <input type="submit" value="Enviar">
-    </form>
+    <div class="container py-3">
+        <h1>{{ $doctor['full_name'] }}</h1>
+        <form action="{{ route("consultation.store") }}" method="post">
+            @csrf
+            @method("post")
+            <div class="form-group">
+                <label for="date">Data</label>
+                <input class="form-control" type="date" name="date" required> <br>
+            </div>
+            <div class="form-group">
+                <label for="time">Horário</label>
+                <input class="form-control" type="time" name="time" required> <br>
+            </div>
+            <div class="form-group">
+                <label for="observations">Observações</label>
+                <textarea class="form-control" name="observations" required placeholder="Digite o que está sentindo..."></textarea>
+            </div>
+            <input type="hidden" name="doctorId" value="{{ $doctor['id'] }}">
+            <div class="my-3">
+                <input type="submit" value="Enviar">
+            </div>
+        </form>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
