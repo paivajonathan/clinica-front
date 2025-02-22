d807862029ec2ae35d578af77e74ca125679540a
Jonathan Paiva <jonathanapaiva@gmail.com>
2025-02-22 01:13:35 -0300

    feat: add cards to dashboard patient

--- resources/views/dashboard-patient.blade.php
+++ resources/views/dashboard-patient.blade.php
@@ -1,42 +1,88 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard - Patient</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
+
 <body>
-    <a href="{{ route("login.destroy") }}">Logout</a>
-    <h1>Welcome to the Patient Dashboard</h1>
-
-    <h2>Available Doctors</h2>
-    <ul>
-        @forelse($doctors as $doctor)
-            <li>
-                {{ $doctor['full_name'] }} - {{ $doctor['specialty'] }}
-                <a href="{{ route('consultation', ['doctorId' => $doctor['id']])  }}">Marcar Consulta</a>
-            </li>
-        @empty
-            <li>No doctors available</li>
-        @endforelse
-    </ul>
-
-    <h2>Pending Consultations</h2>
-    <ul>
-        @forelse($pendingConsultations as $pendingConsultation)
-            <div>
-                {{ $pendingConsultation["doctor_full_name"] }} - {{ $pendingConsultation['date'] }} - {{ $pendingConsultation['time'] }} - {{ $pendingConsultation["status"] }}
-                <form action="{{ route("consultation.cancel", ["consultationId" => $pendingConsultation["id"]]) }}" method="post">
-                    @csrf
-                    @method("put")
-                    <input type="submit" value="Cancelar Consulta" onclick="confirm('Certeza que deseja cancelar?')">
-                </form>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="{{ route("dashboard") }}">Idence</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="{{ route("dashboard") }}">Início</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route("consultation.history") }}">Histórico</a>
+                    </li>
+                </ul>
+                <a class="btn btn-outline-danger" href="{{ route("login.destroy") }}">Logout</a>
             </div>
-        @empty
-            <div>No consultations available</div>
-        @endforelse
-    </ul>
+        </div>
+    </nav>
 
-    <a href="{{ route("consultation.history") }}">Histórico</a>
+    <div class="container py-3">
+        <h2>Médicos Disponíveis</h2>
+        <div class="row">
+            @forelse($doctors as $doctor)
+                <div class="col-sm-3">
+                    <div class="card" style="width: 18rem;">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $doctor['full_name'] }}</h5>
+                            <h5 class="card-subtitle mb-2 text-muted">{{ $doctor['specialty'] }}</h5>
+                            <a href="{{ route('consultation', ['doctorId' => $doctor['id']])  }}"
+                                class="btn btn-primary">Marcar consulta</a>
+                        </div>
+                    </div>
+                </div>
+            @empty
+                <p>No doctors available</p>
+            @endforelse
+        </div>
+    </div>
+
+    <div class="container py-3">
+        <h2>Consultas Pendentes</h2>
+        <div class="row">
+            @forelse($pendingConsultations as $pendingConsultation)
+                <div class="col-sm-3">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $pendingConsultation["doctor_full_name"] }}</h5>
+                            <h5 class="card-subtitle mb-2 text-muted">
+                                {{ $pendingConsultation['date'] }}, às {{ $pendingConsultation['time'] }}
+                            </h5>
+                            <p class="card-text">
+                                {{ $pendingConsultation["status"] }}
+                            </p>
+                            <form
+                                action="{{ route("consultation.cancel", ["consultationId" => $pendingConsultation["id"]]) }}"
+                                method="post">
+                                @csrf
+                                @method("put")
+                                <input class="btn btn-danger" type="submit" value="Cancelar Consulta"
+                                    onclick="confirm('Certeza que deseja cancelar?')">
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            @empty
+                <div>No consultations available</div>
+            @endforelse
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
+
 </html>
