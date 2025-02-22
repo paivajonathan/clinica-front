a3e03feb99a0f049db704fadbe0a14bf6982fb92
Jonathan Paiva <jonathanapaiva@gmail.com>
2025-02-22 20:46:43 -0300

    feat: update config files

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,16 +1,6 @@
 #!/usr/bin/env bash
 echo "Running composer"
-composer global require hirak/prestissimo
 composer install --no-dev --working-dir=/var/www/html
-
-echo "Setting up writable database directory..."
-mkdir -p /tmp
-touch /tmp/database.sqlite
-chmod -R 777 /tmp/database.sqlite
-chown -R www-data:www-data /tmp/database.sqlite
-
-echo "Generating application key..."
-php artisan key:generate --show
 
 echo "Caching config..."
 php artisan config:cache
@@ -19,4 +9,7 @@ echo "Caching routes..."
 php artisan route:cache
 
 echo "Running migrations..."
-php artisan migrate --force
+php artisan migrate --force 
+
+echo "Publishing cloudinary provider..."
+php artisan vendor:publish --provider="CloudinaryLabs\CloudinaryLaravel\CloudinaryServiceProvider" --tag="cloudinary-laravel-config"
