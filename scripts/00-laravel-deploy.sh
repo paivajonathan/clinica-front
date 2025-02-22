660690776d7d20182a568950489df81938037e69
Jonathan Paiva <jonathanapaiva@gmail.com>
2025-02-22 20:08:22 -0300

    feat: update script

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -3,7 +3,12 @@ echo "Running composer"
 composer global require hirak/prestissimo
 composer install --no-dev --working-dir=/var/www/html
 
-echo "generating application key..."
+echo "Setting permissions for database folder..."
+mkdir -p /var/www/html/database
+chmod -R 777 /var/www/html/database
+chown -R www-data:www-data /var/www/html/database
+
+echo "Generating application key..."
 php artisan key:generate --show
 
 echo "Caching config..."
